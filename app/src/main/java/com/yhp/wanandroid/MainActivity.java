f09f9e6def41874f9f83cebf6ad5c9aeb63b0332
yuanhp <yuan.hp@qq.com>
2018-05-11 09:11:39 +0800

    实现MainActivity

--- app/src/main/java/com/yhp/wanandroid/MainActivity.java
+++ app/src/main/java/com/yhp/wanandroid/MainActivity.java
@@ -1,13 +1,64 @@
 package com.yhp.wanandroid;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+    @BindView(R.id.fragment_container)
+    FrameLayout mContatiner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        // 点击ToolBar左侧返回按钮打开关闭DrawerLayout
+        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(
+                this, mDrawerLayout, mToolbar,
+                R.string.drawer_open, R.string.drawer_close);
+        mDrawerToggle.syncState();
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_toolbar_hot:
+                Toast.makeText(this, "hot", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_toolbar_search:
+                Toast.makeText(this, "search", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
